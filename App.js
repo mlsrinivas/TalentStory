d66d3f7786e8770091c99f470656d9898a197fdb
ml srinivas <mlsrinvas70@gmail.com>
2019-08-13 11:50:54 +0530

    changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>TalentStory inital stage </Text>
     </View>
   );
 }
