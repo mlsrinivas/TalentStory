336201ae59c7d27043661a9af85428b7a0f12c96
venkatesh <venkatesh@s7works.io>
2019-09-02 22:05:18 +0530

    small changes

--- src/Components/TalentStoryStar.js
+++ src/Components/TalentStoryStar.js
@@ -3,33 +3,31 @@ import {
   TabBarBottom
 } from "react-navigation";
 import { View, Text, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 //import { LinearGradient } from "expo";
 //import { Ionicons  } from '@expo/vector-icons';
 
 const TalentStoryStar = props => {
   return (
-    <View style={styles.actionButton}
-    >
-       
-          <Image source={require('../../assets/tstar.webp')} tintColor style={styles.buttonIcon}/>
-       
-        {/* <View style={{backgroundColor: "#fff"}}>
-          <TabBarBottom {...props } />
-        </View> */}
-    </View>
+      <View style={{flex:1}}>
+          <TouchableOpacity style={styles.actionButton}>
+            <Image source={require('../../assets/tstar.webp')} tintColor style={styles.buttonIcon}/>
+          </TouchableOpacity>
+      </View>
+   
   );
 };
 
 const styles = {
   actionButton: {
-    flex: 1,
+    //flex: 1,
     backgroundColor: "#0073C0",
     width: 54,
     height: 54,
     borderRadius: 54/2,
     alignItems: 'center',
     justifyContent: 'center',
-    position: "absolute",
+   // position: "absolute",
     bottom: '30%',
     // left: 155,
     // zIndex: 999
