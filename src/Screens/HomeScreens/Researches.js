11281a00c56ab9f5d84299547651c6b09974f2ad
mpraveen4717 <mudumalapraveen546@gmail.com>
2019-11-07 18:07:14 +0530

    updated screens are done

--- src/Screens/HomeScreens/Researches.js
+++ src/Screens/HomeScreens/Researches.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import { TextField } from 'react-native-material-textfield';
+import { View, Text, StyleSheet } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import BottomTabsHeader from '../../Components/ReUsableComponents/HomeScreenHeader/BottomTabsHeader'
 import SearchScreen from '../SearchScreen'
-import Icon from 'react-native-vector-icons/FontAwesome'
-//import { Icon } from 'native-base';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Entypo from 'react-native-vector-icons/Entypo'
+import { Dropdown } from 'react-native-material-dropdown';
 
 export default class Researches extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -16,9 +17,10 @@ export default class Researches extends React.Component {
     this.state = {
       search: false,
       name: '',
-      cardList: [{}, {}, {}, {}]
+      cardList: [ {}, {}, {} ]
     }
-    this.updateParentComponent = this.updateParentComponent.bind(this)
+    this.updateParentComponent = this.updateParentComponent.bind(this);
+    this.dropDownJoin = this.dropDownJoin.bind(this);
   }
 
   updateParentComponent(search) { 
@@ -27,6 +29,12 @@ export default class Researches extends React.Component {
     })
   }
 
+  dropDownJoin(){
+   let data = [{ value:'join',value:'st5udy' }]
+     return (<View>
+             <Dropdown  data={data} itemCount = '1'/>
+             </View>) 
+  }
 
   renderAddCommunity() {
     const name = this.state;
@@ -36,17 +44,17 @@ export default class Researches extends React.Component {
         <View style = {{flexDirection:'row'}}>
           <View style={ styles.inputstyle1 }>
           <TouchableOpacity>
-          <Icon name='search' size={20} color='rgb(111,111,111)' style={{ margin:'3%' }}/>
+          <MaterialIcons name='search' size={28} color='rgb(111,111,111)' style={{ margin:'3%' }}/>
           </TouchableOpacity>
           </View>
         </View>
 
 
-        <View style = {{flexDirection: 'row',marginTop:'2%', justifyContent:'space-around'}}>
-        <TouchableOpacity style = {{marginLeft:'22%', }}
+        <View style = {{flexDirection: 'row',marginTop:'2%', justifyContent:'space-between'}}>
+        <TouchableOpacity style = {{marginLeft:'15%', }}
                           onPress = {() => {this.props.navigation.navigate('MyResearches')}}>
         <View style = {styles.inputstyle2}>
-        <Text style = {{color:'black', marginTop:'4%', alignSelf:'center', fontSize:15}}>My Researches</Text>
+        <Text style = {{color:'black', marginTop:'5%', alignSelf:'center', fontSize:15}}>My Researches</Text>
         </View>
         </TouchableOpacity>
         <TouchableOpacity style = {{marginRight:'15%', }}
@@ -67,27 +75,34 @@ export default class Researches extends React.Component {
           {this.state.cardList.map((lists,index) => (
           <View style = {styles.cardView}>
               <View style = {{marginLeft:'2%'}}>
-                <Text style = {{color:'#B2B2B2', fontSize:12, marginTop:'1%'}}>Created On September 1,21:12</Text>
-                <Text style = {{color:'black', fontSize:15, marginTop:'1%'}}>Monsoon TechVibes</Text>
-                <Text style = {{color:'#B2B2B2', fontSize:12, marginTop:'1%'}}>the course of the day ra clarity on the phone with them now it's showing the house is in office today's Birthday and Happy</Text>
-                <Text style = {{color:'#B2B2B2', fontSize:15, marginTop:'2%'}}>Three Weeks</Text>
-                <Text style = {{color:'#B2B2B2', fontSize:15, marginTop:'2%'}}>345</Text>
-                <Text style = {{color:'red', fontSize:15, marginTop:'2%'}}>Free/-</Text>
+                <View style={{ flexDirection:'row' }}>
+                <Text style = {{color:'#B2B2B2', fontSize:12, marginTop:'3%',marginLeft:'3%' }}>Created On September 1,21:12</Text>
+                <Entypo name='dots-three-vertical' size={20} style={{ marginHorizontal:'33%',marginTop:'2%' }}/>
+                </View>
+                <Text style = {{color:'black', fontSize:15, marginTop:'1%',marginLeft:'3%' }}>Monsoon TechVibes</Text>
+                <Text style = {{color:'#B2B2B2', fontSize:12, marginTop:'1%',marginLeft:'3%' }}>the course of the day ra clarity on the phone with them now it's showing the house is in office today's Birthday and Happy</Text>
+                <View style={{ flexDirection:'row' }}>
+                <Text style = {{color:'#B2B2B2', fontSize:15, marginTop:'2%',marginLeft:'3%' }}>Three Weeks</Text>          
+                <Entypo name='dot-single' size={24} style={{ marginTop:'2%',marginLeft:'1%',color:'#B2B2B2' }}/>
+                <Text style={{ color:'#B2B2B2', fontSize:15, marginTop:'2%' }}>27-07-2019</Text>
+                </View>
+                <Text style = {{color:'#B2B2B2', fontSize:15, marginTop:'2%',marginLeft:'3%' }}>345</Text>
+                <Text style = {{color:'orange', fontSize:15, marginTop:'2%',marginLeft:'3%' }}>Free/-</Text>
               </View>
 
             <View style = {{flexDirection:'row', justifyContent:'space-between', marginTop:'3%'}}>
-              <View style = {{height:25, borderWidth:1, borderColor:'rgb(240,240,240)'}}>
-                <Text style = {{margin:'2%', color:'#744E98', }}>#View</Text>
+              <View style = {{height:25, borderWidth:1, borderColor:'rgb(240,240,240)',width:'18%'}}>
+                <Text style = {{textAlign:'center',marginTop:'3%', color:'#744E98' }}>#View</Text>
               </View>
-              <View style = {{height:25, borderWidth:1, borderColor:'rgb(240,240,240)',}}>
-                <Text style = {{margin:'2%', color:'black' }}>Pending</Text>
+              <View style = {{height:25, borderWidth:1, borderColor:'rgb(240,240,240)',width:'25%'}}>
+                <Text style = {{ textAlign:'center',marginTop:'3%', color:'black',fontWeight:'bold' }}>Pending</Text>
               </View>
             </View>
 
           </View>
           ))}
         </ScrollView>
-
+          
       </View>
     )
   }
@@ -107,6 +122,14 @@ export default class Researches extends React.Component {
             this.state.search == true && <SearchScreen />
             }  
           {this.state.search == false && this.renderAddCommunity()}
+          { this.state.search == false && <View style={{position: 'absolute', bottom: 20, right:20}}>
+       <TouchableOpacity>
+          <View style={{backgroundColor:'#fff', width:60, height:60, borderRadius: 30, justifyContent: 'center', alignItems: 'center',elevation:5,shadowColor:'#000000',shadowOpacity: 0.25,
+      shadowRadius: 1.84,shadowOffset:{ width:0, height:2 }, }}>
+            <Ionicons name='ios-add'size={29} color='black' onPress={()=>this.props.navigation.navigate('CreateResearch')} />
+          </View>
+        </TouchableOpacity>
+      </View>}
         </View>
         
         
@@ -117,22 +140,28 @@ export default class Researches extends React.Component {
 const styles = StyleSheet.create({
   inputstyle1:{
       height:45,
-      borderColor:'#D3D3D3',
+      borderColor:'#f5f5f5',
       borderWidth:1, 
       width:'96%',
       borderRadius:2,
       paddingHorizontal:15,
-      marginTop:7,
+      marginTop:'2%',
       marginLeft:'2%',
-      backgroundColor: 'rgb(240,240,240)',
+      marginRight:'2%',
+      backgroundColor: '#f5f5f5',
+      shadowColor: '#000000',
+      shadowOffset:{ width:0, height:2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 1.84,
+      elevation: 3,
   },
   inputstyle2:{
     height:40,
     borderColor:'black',
     borderWidth:1, 
-    width:'90%',
     borderRadius:5,
-    backgroundColor: 'rgb(240,240,240)',
+    backgroundColor: '#f5f5f5',
+    width:'100%'
 },
 inputstyle3:{
   height:25,
